2eebe6ed6493840dd496b58e5eec436fa42ef9ac
msj0319 <gaeddongie13@gmail.com>
2021-08-17 21:12:16 +0900

    first commit

---
+++ src/test/java/net/msj0319/api/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.msj0319.api.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
